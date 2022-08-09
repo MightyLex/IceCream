3c939feb98bebb7c168ddec4002546c3cc7f601a
apieceoflego <106179672+apieceoflego@users.noreply.github.com>
2022-08-09 07:27:46 +0000

    Deploying to gh-pages from @ apieceoflego/team-project-icecream@86337ee4dba0d2e1acba134483a2c2ef13c43dca 🚀

---
+++ index.63496f11.js
@@ -0,0 +1,2 @@
+$(".multiple-items").slick({infinite:!0,slidesToShow:1,slidesToScroll:1,arrows:!1,dots:!0});
+//# sourceMappingURL=index.63496f11.js.map
---
+++ index.ba0f9ce7.js
@@ -0,0 +1,2 @@
+$(".multiple-items").slick({infinite:!0,slidesToShow:1,slidesToScroll:1,arrows:!1,dots:!0});
+//# sourceMappingURL=index.ba0f9ce7.js.map
