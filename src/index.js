e36fb8b1ff52e2b034889c5d22ae029f56764c1c
apieceoflego <olehprychshepchuk@gmail.com>
2022-08-10 17:39:32 +0200

    add scrol

--- src/index.js
+++ src/index.js
@@ -53,3 +53,28 @@ $('.multiple-items').slick({
     bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
+
+// Прокрутка при клике
+const menuLinks = document.querySelectorAll('.header__link[data-goto]');
+if (menuLinks.length > 0) {
+  menuLinks.forEach(menuLink => {
+    menuLink.addEventListener('click', onMenuLinkClick);
+  });
+
+  function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    if (
+      menuLink.dataset.goto &&
+      document.querySelector(menuLink.dataset.goto)
+    ) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top;
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: 'smooth',
+      });
+      e.preventDefault();
+    }
+  }
+}
