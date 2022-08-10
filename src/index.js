c5588a6d148d5929322dc96c2b718e4b43a89da5
AnastasiyaLytvyn <107053207+AnastasiyaLytvyn@users.noreply.github.com>
2022-08-10 22:48:39 +0300

    add list

--- src/index.js
+++ src/index.js
@@ -78,3 +78,25 @@ if (menuLinks.length > 0) {
     }
   }
 }
+
+// випадаючий список локації
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
