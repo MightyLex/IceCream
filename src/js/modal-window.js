72393e13bf86941a2f0645a6b88ac40f2fd0e52f
apieceoflego <olehprychshepchuk@gmail.com>
2022-08-11 19:57:46 +0200

    Add modal window

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -1,8 +1,25 @@
+// CONTACTS MODAL
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
+// HEADER MODAL
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[modal-open]'),
+    closeModalBtn: document.querySelector('[modal-close]'),
+    modal: document.querySelector('[modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
