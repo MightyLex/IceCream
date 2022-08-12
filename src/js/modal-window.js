afaf02fb4e25bfecb5abb2e4810ef9cfae3db11e
apieceoflego <olehprychshepchuk@gmail.com>
2022-08-12 13:11:41 +0200

    add modal in mob menu

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -29,3 +29,18 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-opening]'),
+    closeModalBtn: document.querySelector('[data-modal-closing]'),
+    modal: document.querySelector('[data-modal-window]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
